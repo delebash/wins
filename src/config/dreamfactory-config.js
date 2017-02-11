33cd4791d736bce02d808f9cd89ee0688c9dac5e
Daniel Elebash <danelebash@hotmail.com>
2017-02-11 15:49:51 -0500

    config

--- src/config/dreamfactory-config.js
+++ src/config/dreamfactory-config.js
@@ -2,27 +2,27 @@ export default {
   //--------------------------------------------------------------------------
 //  DreamFactory 2.0 instance specific constants
 //--------------------------------------------------------------------------
-  INSTANCE_URL:'https://api.test.com',
-  APP_API_KEY: 'key',
+  INSTANCE_URL: 'https://api.test.com',
+  APP_API_KEY: 'test',
   APP_NAME: 'northwind-app',
-  email:'test@test.com',
+  email: 'test@test.com',
   password: 'test12345',
   api: '/api/v2/',
-  db:'northwind/',
-  service:'_table/',
-  serviceObject:'customers',
+  db: 'northwind/',
+  service: '_table/',
+  serviceObject: 'customers',
   tokenKey: 'token',
-  overrideMethod:'?method=GET',
+  overrideMethod: '?method=GET',
   dataurl: function () {
-  return this.INSTANCE_URL + this.api + this.db + this.service + this.serviceObject + this.overrideMethod;
-},
-  database: function () {
+    return this.baseurl() + this.serviceObject + this.overrideMethod;
+  },
+  baseurl: function () {
     return this.INSTANCE_URL + this.api + this.db + this.service;
   },
   loginurl: function () {
     return this.INSTANCE_URL + '/api/v2/user/session'
   },
   credentials: function () {
-    return {email:this.email, password:this.password}
+    return {email: this.email, password: this.password}
   }
 };
